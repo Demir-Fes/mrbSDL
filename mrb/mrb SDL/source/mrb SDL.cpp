cbd02898486eb72a7c41ba4824d1a7b5a211490f
Demir-Fes <demir.fes@gmail.com>
2016-05-19 17:14:45 +0300

    init

---
+++ mrb/mrb SDL/source/mrb SDL.cpp
@@ -0,0 +1,12 @@
+//============================================================================
+// Name        : mrb.cpp
+// Author      : Demir Fes
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Merhaba SDL!" << endl; // prints Merhaba SDL!
+	return 0;
+}
