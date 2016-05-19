0f2136b3812f3812dab1b339bdb69f8720276620
Demir-Fes <demir.fes@gmail.com>
2016-05-19 18:47:57 +0300

    SDL skeleton test

--- mrb/mrb SDL/source/mrb SDL.cpp
+++ mrb/mrb SDL/source/mrb SDL.cpp
@@ -3,11 +3,37 @@
 // Author      : Demir Fes
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include<SDL.h>
 
-int main() {
-	cout << "Merhaba SDL!" << endl; // prints Merhaba SDL!
+SDL_Window* window = 0;
+SDL_Renderer* renderer = 0;
+
+int main(int argc, char* args[]) {
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+
+		window = SDL_CreateWindow("mrb SDL",
+				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480,
+				SDL_WINDOW_SHOWN);
+
+		if (window != 0) {
+			renderer = SDL_CreateRenderer(window, -1, 0);
+		}
+	} else {
+		return 1;
+	}
+
+
+
+	SDL_SetRenderDrawColor(renderer, 255, 222, 222, 255);
+
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(2000);
+
+	SDL_Quit();
 
 	return 0;
 }
